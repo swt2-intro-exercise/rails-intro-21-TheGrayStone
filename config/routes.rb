a4ee033f9052c4e2a20dff33747084dc013456de
TheGrayStone <nilslissner@gmx.de>
2021-11-04 18:43:16 +0100

    Fix #2

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,15 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
+  get 'new_author' => 'new_author#index'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-  
+
   # Example resource route with options:
   #   resources :products do
   #     member do
