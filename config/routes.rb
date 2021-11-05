70c509b34d343a4722e0d73ca403b5874e79c34b
TheGrayStone <nilslissner@gmx.de>
2021-11-05 11:49:32 +0100

    reintroduce new_author_path (Fix #8)

--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,8 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  get 'authors/new' => 'author#new'
-  post 'authors/new' => 'author#create'
+  get 'authors/new', to: 'author#new', as: 'new_author'
+  post 'authors/new', to: 'author#create'
   get 'authors/show/:id', to: 'author#show', as: 'authors'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
